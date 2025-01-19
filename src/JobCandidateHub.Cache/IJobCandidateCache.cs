69a44d2f8671c0fca46b837c782cab45a5bc18b0
Sandip Bantawa <sandip_ultimate@hotmail.com>
2025-01-19 22:49:19 +0545

    Added Cache Service

---
+++ src/JobCandidateHub.Cache/IJobCandidateCache.cs
@@ -0,0 +1,8 @@
+﻿namespace JobCandidateHub.Cache
+{
+	public interface IJobCandidateCache<T>
+	{
+		public T GetCache(string key);
+		public void SetCache(string key, T value);
+	}
+}
---
+++ src/JobCandidateHub.Cache/JobCandidateDictionaryCache.cs
@@ -0,0 +1,32 @@
+﻿namespace JobCandidateHub.Cache
+{
+	public class JobCandidateDictionaryCache<T> : IJobCandidateCache<T>
+	{
+		private static IDictionary<string, T> _cache;
+		public JobCandidateDictionaryCache()
+		{
+			if (_cache == null)
+			{
+				_cache = new Dictionary<string, T>();
+			}
+		}
+
+		public T GetCache(string key)
+		{
+			_cache.TryGetValue(key, out T cachedItem);
+			return cachedItem;
+		}
+
+		public void SetCache(string key, T value)
+		{
+			if (GetCache(key) == null)
+			{
+				_cache.Add(key, value);
+			}
+			else
+			{
+				_cache[key] = value;
+			}
+		}
+	}
+}
