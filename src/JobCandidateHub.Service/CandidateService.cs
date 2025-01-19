53167b841639a44dab5ed31b48e3cc74e42404ed
Sandip Bantawa <sandip_ultimate@hotmail.com>
2025-01-19 23:06:25 +0545

    Added Services

---
+++ src/JobCandidateHub.Service/CandidateService.cs
@@ -0,0 +1,34 @@
+﻿using JobCandidateHub.Cache;
+using JobCandidateHub.Data.Repository;
+using JobCandidateHub.Model.Common;
+using JobCandidateHub.Model.Db;
+
+namespace JobCandidateHub.Service
+{
+	public class CandidateService : ICandidateService
+	{
+		private readonly ICandidateRepository _candidateRepository;
+		private readonly IJobCandidateCache<Candidate> _cache;
+		public CandidateService(ICandidateRepository candidateRepository)
+		{
+			_candidateRepository = candidateRepository;
+			_cache = new JobCandidateDictionaryCache<Candidate>();
+		}
+
+		public async Task<EnumHelpers.EOpResult> AddOrUpdateCandidate(Candidate candidate)
+		{
+			var oCandidate = _cache.GetCache(candidate.Email) ?? await _candidateRepository.GetCandidateAsync(candidate.Email);
+			var dbStatus = EnumHelpers.EOpResult.Processing;
+			if (oCandidate == null)
+			{
+				dbStatus = await _candidateRepository.SaveCandidate(candidate);
+			}
+			else
+			{
+				dbStatus = await _candidateRepository.UpdateCandidate(oCandidate);
+			}
+			_cache.SetCache(candidate.Email, candidate);
+			return dbStatus;
+		}
+	}
+}
---
+++ src/JobCandidateHub.Service/ICandidateService.cs
@@ -0,0 +1,10 @@
+﻿using JobCandidateHub.Model.Common;
+using JobCandidateHub.Model.Db;
+
+namespace JobCandidateHub.Service
+{
+	public interface ICandidateService
+	{
+		Task<EnumHelpers.EOpResult> AddOrUpdateCandidate(Candidate candidate);
+	}
+}
