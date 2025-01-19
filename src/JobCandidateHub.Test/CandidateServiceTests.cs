a23e348388ccef6363f43a737f3429a9a6734d0f
Sandip Bantawa <sandip_ultimate@hotmail.com>
2025-01-20 01:10:47 +0545

    Added tests

---
+++ src/JobCandidateHub.Test/CandidateServiceTests.cs
@@ -0,0 +1,41 @@
+﻿using JobCandidateHub.Data.Repository;
+using JobCandidateHub.Model.Common;
+using JobCandidateHub.Model.Db;
+using JobCandidateHub.Service;
+using Moq;
+
+namespace JobCandidateHub.Test
+{
+	public class CandidateServiceTests
+	{
+		[Fact]
+		public async Task AddOrUpdateCandidate_No_Existing_Candidate_Returns_Added()
+		{
+			var candidateModel = new Candidate { Email = "abc1@gmail.com" };
+			var candidateRepositoryMock = new Mock<ICandidateRepository>();
+			candidateRepositoryMock.Setup(r => r.UpdateCandidateAsync(It.IsAny<Candidate>())).ReturnsAsync(EnumHelpers.EOpResult.Updated);
+			candidateRepositoryMock.Setup(r => r.SaveCandidateAsync(It.IsAny<Candidate>())).ReturnsAsync(EnumHelpers.EOpResult.Added);
+			candidateRepositoryMock.Setup(r => r.GetCandidateAsync(It.IsAny<string>())).ReturnsAsync(() => null);
+
+			ICandidateService candidateService = new CandidateService(candidateRepositoryMock.Object);
+			var dbStatus = await candidateService.AddOrUpdateCandidateAsync(candidateModel);
+
+			Assert.Equal(EnumHelpers.EOpResult.Added, dbStatus);
+		}
+
+		[Fact]
+		public async Task AddOrUpdateCandidate_Having_Existing_Candidate_Returns_Updated()
+		{
+			var candidateModel = new Candidate { Email = "abc@gmail.com" };
+			var candidateRepositoryMock = new Mock<ICandidateRepository>();
+			candidateRepositoryMock.Setup(r => r.UpdateCandidateAsync(It.IsAny<Candidate>())).ReturnsAsync(EnumHelpers.EOpResult.Updated);
+			candidateRepositoryMock.Setup(r => r.SaveCandidateAsync(It.IsAny<Candidate>())).ReturnsAsync(EnumHelpers.EOpResult.Added);
+			candidateRepositoryMock.Setup(r => r.GetCandidateAsync(It.IsAny<string>())).ReturnsAsync(candidateModel);
+
+			ICandidateService candidateService = new CandidateService(candidateRepositoryMock.Object);
+			var dbStatus = await candidateService.AddOrUpdateCandidateAsync(candidateModel);
+
+			Assert.Equal(EnumHelpers.EOpResult.Updated, dbStatus);
+		}
+	}
+}
---
+++ src/JobCandidateHub.Test/CandidatesControllerTests.cs
@@ -0,0 +1,64 @@
+﻿using JobCandidateHub.Api.Controllers;
+using JobCandidateHub.Api.Model;
+using JobCandidateHub.Model.Common;
+using JobCandidateHub.Model.Db;
+using JobCandidateHub.Service;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System.ComponentModel.DataAnnotations;
+
+namespace JobCandidateHub.Test
+{
+	public class CandidatesControllerTests
+	{
+		[Fact]
+		public async Task Validate_Invalid_Model_Returns_Invalid()
+		{
+			var candidateModel = new CandidateModel { Email = "abc@gmail.com", FirstName = "", LastName = "lastName", Comment = "comment" };
+
+			var context = new ValidationContext(candidateModel, null, null);
+			var results = new List<ValidationResult>();
+			var isModelStateValid = Validator.TryValidateObject(candidateModel, context, results, true);
+
+			Assert.False(isModelStateValid);
+		}
+
+		[Fact]
+		public async Task Validate_Valid_Model_Returns_Valid()
+		{
+			var candidateModel = new CandidateModel { Email = "abc@gmail.com", FirstName = "firstName", LastName = "lastName", Comment = "comment" };
+
+			var context = new ValidationContext(candidateModel, null, null);
+			var results = new List<ValidationResult>();
+			var isModelStateValid = Validator.TryValidateObject(candidateModel, context, results, true);
+
+			Assert.True(isModelStateValid);
+		}
+
+		[Fact]
+		public async Task Post_Success_AddOrUpdate_Returns_Ok()
+		{
+			var candidateServiceMock = new Mock<ICandidateService>();
+			candidateServiceMock.Setup(r => r.AddOrUpdateCandidateAsync(It.IsAny<Candidate>())).ReturnsAsync(EnumHelpers.EOpResult.Added);
+			var candidateModel = new CandidateModel { Email = "abc@gmail.com", FirstName = "firstName", LastName = "lastName", Comment = "comment" };
+
+			var candidatesController = new CandidatesController(candidateServiceMock.Object);
+			var result = await candidatesController.Post(candidateModel) as ObjectResult;
+
+			Assert.Equal(200, result.StatusCode);
+		}
+
+		[Fact]
+		public async Task Post_Failed_AddOrUpdate_Returns_InternalServerError()
+		{
+			var candidateServiceMock = new Mock<ICandidateService>();
+			candidateServiceMock.Setup(r => r.AddOrUpdateCandidateAsync(It.IsAny<Candidate>())).ReturnsAsync(EnumHelpers.EOpResult.Failed);
+			var candidateModel = new CandidateModel { Email = "abc@gmail.com", FirstName = "firstName", LastName = "lastName", Comment = "comment" };
+
+			var candidatesController = new CandidatesController(candidateServiceMock.Object);
+			var result = await candidatesController.Post(candidateModel) as ObjectResult;
+
+			Assert.Equal(500, result.StatusCode);
+		}
+	}
+}
